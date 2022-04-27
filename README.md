3e764ec3ca6d905461b20e1bec74c366d7f778ff
Joseph Rollins <rollins.joseph+git@gmail.com>
2022-04-27 21:22:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# SBP Tracker
+
+SBP Tracker charts the rolling 7 day occupancy stats for all 3 Seattle Bouldering Project gym locations.
+
+Seattle Bouldering Project is a collection of 3 bouldering gyms in the Seattle area. After re-opening post COVID they published semi real-time occupancy counts at each of their locations on their [website](https://seattleboulderingproject.com/occupancy). These give an estimate of the number of people at the gym at that moment.
+
+![Preview of the SBP Tracker chart](preview.png)
---
+++ public/index.js
@@ -0,0 +1,73 @@
+fetch('/stats')
+  .then(r => r.json())
+  .then(render);
+
+const COLORS = {
+  POP: 'rgba(255, 99, 132, 0.4)',
+  UPW: 'rgba(54, 162, 235, 0.4)',
+  FRE: 'rgba(255, 206, 86, 0.4)',
+};
+
+const NAMES = {
+  POP: 'Poplar',
+  UPW: 'Upper Walls',
+  FRE: 'Fremont',
+};
+
+function render(stats) {
+  stats = stats.reduce((acc, v) => {
+    const keys = Object.keys(v);
+    const time = v['time'];
+    for (const key of keys) {
+      if (key === 'time')
+        continue;
+
+      if (!acc[key]) {
+        acc[key] = {
+          label: NAMES[key],
+          borderColor: COLORS[key],
+          backgroundColor: COLORS[key],
+          pointRadius: 0.2,
+          tension: 0.1,
+          fill: false,
+          data: []
+        }
+      }
+      acc[key].data.push({
+        x: time,
+        y: v[key],
+      });
+    }
+    return acc;
+  }, {});
+  
+  const chartConfig = {
+    type: 'line',
+    data: {
+      datasets: Object.values(stats),
+    },
+    options: {
+      scales: {
+        x: {
+          type: 'time',
+        }
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: 'Occupancy at Seattle Bouldering Project Locations',
+        },
+        subtitle: {
+          display: true,
+          text: 'Rolling 7 day view with data updated every 5 minutes',
+        }
+      },
+      interaction: {
+        mode: 'index',
+        intersect: false,
+      },
+    },
+  }
+
+  new Chart(document.getElementById('chart'), chartConfig);
+}
